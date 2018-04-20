357e6deb7736d65998c49d2049b3538631ea07fc
wangmeng <510782645@qq.com>
2018-04-20 13:08:12 +0800

    修改安装部署的文件

--- blogProject/wsgi.py
+++ blogProject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogProject.settings")
 
 application = get_wsgi_application()
+sys.path.append('/usr/lib/python2.7/site-packages')
+sys.path.append('/usr/lib64/python2.7/site-packages')
